7bbac0a89c13665a5da528d88e7bd135d69c93a5
Pedro Veloso <pedroveloso@squareup.com>
2023-02-01 17:37:24 -0800

    Setup stub app with Spotless and other common Android dependencies

---
+++ app/src/main/java/app/cash/paykit/sample/ui/MainActivity.kt
@@ -0,0 +1,19 @@
+package app.cash.paykit.sample.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.cash.paykit.sample.R
+import app.cash.paykit.sample.ui.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    if (savedInstanceState == null) {
+      supportFragmentManager.beginTransaction()
+        .replace(R.id.container, MainFragment.newInstance())
+        .commitNow()
+    }
+  }
+}
---
+++ app/src/main/java/app/cash/paykit/sample/ui/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package app.cash.paykit.sample.ui.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+  // TODO: Implement the ViewModel
+}
