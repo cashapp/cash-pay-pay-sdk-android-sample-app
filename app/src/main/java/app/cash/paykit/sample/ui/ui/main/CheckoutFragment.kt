44f7c1638411bf8a93f142e8c37ab59dec87f0a6
Pedro Veloso <pedroveloso@squareup.com>
2023-02-02 17:06:51 -0800

    Integrate with Cash PayKit SDK

--- app/src/main/java/app/cash/paykit/sample/ui/ui/main/CheckoutFragment.kt
+++ app/src/main/java/app/cash/paykit/sample/ui/ui/main/CheckoutFragment.kt
@@ -1,12 +1,32 @@
 package app.cash.paykit.sample.ui.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import app.cash.paykit.core.PayKitState.Approved
+import app.cash.paykit.core.PayKitState.Authorizing
+import app.cash.paykit.core.PayKitState.CreatingCustomerRequest
+import app.cash.paykit.core.PayKitState.Declined
+import app.cash.paykit.core.PayKitState.NotStarted
+import app.cash.paykit.core.PayKitState.PayKitException
+import app.cash.paykit.core.PayKitState.PollingTransactionStatus
+import app.cash.paykit.core.PayKitState.ReadyToAuthorize
+import app.cash.paykit.core.PayKitState.RetrievingExistingCustomerRequest
+import app.cash.paykit.core.PayKitState.UpdatingCustomerRequest
+import app.cash.paykit.core.models.sdk.PayKitCurrency.USD
+import app.cash.paykit.core.models.sdk.PayKitPaymentAction.OneTimeAction
+import app.cash.paykit.sample.R
 import app.cash.paykit.sample.databinding.FragmentCheckoutBinding
+import kotlinx.coroutines.launch
 
 class CheckoutFragment : Fragment() {
 
@@ -32,7 +52,74 @@ class CheckoutFragment : Fragment() {
     _binding = null
   }
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    viewModel.initializeSDK()
+    val oneTimePayment = OneTimeAction(redirectURI, USD, 825, sandboxBrandID)
+    viewModel.createOneTimePayment(oneTimePayment)
+  }
+
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+    handlePayKitStateChanges()
+    binding.payKitButton.setOnClickListener {
+      viewModel.authorizeCustomerRequest(requireContext())
+    }
+  }
+
+  private fun handlePayKitStateChanges() {
+    lifecycleScope.launch {
+      repeatOnLifecycle(Lifecycle.State.STARTED) {
+        viewModel.payKitState.collect { newState ->
+          when (newState) {
+            is Approved -> {
+              // Use `newState.responseData` to retrieve the information necessary to send to your own backend.
+              showToast(R.string.state_approved)
+            }
+
+            Authorizing -> {
+              // Use this state to display loading status if desired.
+            }
+
+            CreatingCustomerRequest -> {
+              // Use this state to display loading status if desired.
+            }
+
+            Declined -> {
+              showToast(R.string.state_declined)
+            }
+
+            NotStarted -> {
+              binding.payKitButton.isEnabled = false
+            }
+
+            is PayKitException -> {
+              showToast(R.string.state_error)
+              Log.e("Sample", "Exception occurred: ${newState.exception}")
+            }
+            PollingTransactionStatus -> {
+              // Use this state to display loading status if desired.
+            }
+
+            is ReadyToAuthorize -> {
+              binding.payKitButton.isEnabled = true
+            }
+
+            UpdatingCustomerRequest -> {
+              // Use this state to display loading status if desired.
+            }
+
+            RetrievingExistingCustomerRequest -> {
+              // Use this state to display loading status if desired.
+              // Used only when retrieving existing customer request. Corner case (eg.: app killed)
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private fun showToast(@StringRes stringRes: Int) {
+    Toast.makeText(requireContext(), stringRes, Toast.LENGTH_SHORT).show()
   }
 }
--- app/src/main/java/app/cash/paykit/sample/ui/ui/main/MainViewModel.kt
+++ app/src/main/java/app/cash/paykit/sample/ui/ui/main/MainViewModel.kt
@@ -1,20 +1,72 @@
 package app.cash.paykit.sample.ui.ui.main
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import app.cash.paykit.core.CashAppPayKit
+import app.cash.paykit.core.CashAppPayKitFactory
+import app.cash.paykit.core.CashAppPayKitListener
+import app.cash.paykit.core.PayKitState
+import app.cash.paykit.core.PayKitState.ReadyToAuthorize
+import app.cash.paykit.core.models.sdk.PayKitPaymentAction
 import app.cash.paykit.sample.ui.ui.main.Screens.CHECKOUT
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 enum class Screens {
   ADD_TO_CART,
   CHECKOUT,
 }
-class MainViewModel : ViewModel() {
 
+/* Change these IDs and redirect URI accordingly for your own app. */
+private const val sandboxClientID = "CAS-CI_PAYKIT_MOBILE_DEMO"
+const val sandboxBrandID = "BRAND_9t4pg7c16v4lukc98bm9jxyse"
+
+const val redirectURI = "cashpaykitsample://checkout"
+
+class MainViewModel : ViewModel(), CashAppPayKitListener {
+
+  // Used for simple navigation.
   private val _currentScreen = MutableStateFlow(Screens.ADD_TO_CART)
   val currentScreen: StateFlow<Screens> = _currentScreen
 
+  // Cash PayKit State.
+  private val _payKitState = MutableStateFlow<PayKitState>(PayKitState.NotStarted)
+  val payKitState: StateFlow<PayKitState> = _payKitState.asStateFlow()
+
+  private lateinit var payKitSdk: CashAppPayKit
+  var currentRequestId: String? = null
+    private set
+
+  override fun payKitStateDidChange(newState: PayKitState) {
+    // Store the `requestId`.
+    if (newState is ReadyToAuthorize) {
+      currentRequestId = newState.responseData.id
+    }
+
+    // Propagate state so consumers of this ViewModel can be notified of changes.
+    _payKitState.value = newState
+  }
+
   fun navigateToCheckoutScreen() {
     _currentScreen.value = CHECKOUT
+  }
+
+  fun initializeSDK() {
+    payKitSdk = CashAppPayKitFactory.createSandbox(sandboxClientID)
+    payKitSdk.registerForStateUpdates(this)
+  }
+
+  fun createOneTimePayment(paymentAction: PayKitPaymentAction) {
+    viewModelScope.launch(Dispatchers.IO) {
+      payKitSdk.createCustomerRequest(paymentAction)
+    }
+  }
+
+  fun authorizeCustomerRequest(context: Context) {
+    payKitSdk.authorizeCustomerRequest(context)
   }
 }
